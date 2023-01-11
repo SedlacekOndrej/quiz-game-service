1ce5eb7e84e00c3abf4248c1cc92c42f6c70ef93
SedlacekOndrej <SedlacekOndrej@seznam.cz>
2023-01-11 18:23:56 +0100

    created answer generation function

--- src/main/java/com/sedlacek/quiz/services/CapitalService.java
+++ src/main/java/com/sedlacek/quiz/services/CapitalService.java
@@ -25,8 +25,19 @@ public class CapitalService {
     }
 
     public String renderEuropeanCapitals(Model model) {
+        List<String> states = generateRandomStates();
         model.addAttribute("loggedUser", userService.tryGetLoginSessionUser());
-        model.addAttribute("states", generateRandomStates());
+        model.addAttribute("states", states);
+        model.addAttribute("answers1", generateAnswers(states.get(0)));
+        model.addAttribute("answers2", generateAnswers(states.get(1)));
+        model.addAttribute("answers3", generateAnswers(states.get(2)));
+        model.addAttribute("answers4", generateAnswers(states.get(3)));
+        model.addAttribute("answers5", generateAnswers(states.get(4)));
+        model.addAttribute("answers6", generateAnswers(states.get(5)));
+        model.addAttribute("answers7", generateAnswers(states.get(6)));
+        model.addAttribute("answers8", generateAnswers(states.get(7)));
+        model.addAttribute("answers9", generateAnswers(states.get(8)));
+        model.addAttribute("answers10", generateAnswers(states.get(9)));
         return "european-capitals";
     }
 
@@ -41,6 +52,21 @@ public class CapitalService {
             }
         }
         return generatedStates;
+    }
+
+    public List<String> generateAnswers(String state) {
+        Random random = new Random();
+        List<String> generatedAnswers = new ArrayList<>();
+        generatedAnswers.add(europeanCapitals.getCapitals().get(state));
+        List<String> capitals = europeanCapitals.getCapitals().values().stream().toList();
+        while (generatedAnswers.size() < 4) {
+            String generatedAnswer = capitals.get(random.nextInt(50));
+            if (!generatedAnswers.contains(generatedAnswer)) {
+                generatedAnswers.add(generatedAnswer);
+            }
+        }
+        Collections.shuffle(generatedAnswers);
+        return generatedAnswers;
     }
 
     public boolean rightAnswer(String state, String capital) {
